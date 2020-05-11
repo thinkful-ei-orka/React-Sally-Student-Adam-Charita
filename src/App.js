4f15ed658425fa0289f98d42f0a61f07a07779f9
Adam Brown <adam.edward.brown@gmail.com>
2020-05-11 13:53:47 -0400

    content entered, css and semantic to be done later

--- src/App.js
+++ src/App.js
@@ -2,25 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function contentHeader() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className='header' id='header'>
+      <h1 className='studentName'>
+        Sally Student
+      </h1>
+      <h4 className='tag'>
+        Full stack developer excited to help companies achieve their visions in the modern world.
+      </h4>
+    </header>
   );
 }
 
-export default App;
+function contentMain(){
+  return (
+    <main>
+      <section>
+        <h2>Education</h2>
+        <ul>
+          <li>North Hall High School - 3.8 GPA</li>
+          <li>Georgia State University - Bachelor's in Health Informatics</li>
+          <li>Thinkful - Engineering Immersion</li>
+        </ul>
+      </section>
+      <section>
+        <h2>Work History</h2>
+        <p>
+          <b>Walmart - Assistant Manager</b><br />
+          <i>Jun 2016-Feb 2018</i><br />
+          Work up shift schedules, train new employees, oversee inventory and ordering of new stock, handling high-level customer issues, checking out and counting drawers of registers<br />
+          <br />
+          <b>US Army - Military Secretary</b><br />
+          <i>Mar 2018-Nov 2019</i><br />
+          Filing and managing of military records for Col. Phil Darnell. Assisted the Col in disseminating information to subordinates, and performing various other military duties when required. Also handled scheduling meetings, and office management in the Colonel's absence.<br />
+        </p>
+      </section>
+    </main>
+  )
+}
+
+function contentFooter(){
+  return (
+    <footer>
+      <address>
+        Sally Student<br />
+        <a href="mailto:sallystudent@gmail.com" target="_blank">sallystudent@gmail.com</a><br />
+        <a href="tel:+14042230987">(404)223-0987</a><br />
+        <img src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/05/29/16/ww-11.jpg?w968h681" alt="Action Shot" />
+      </address>
+    </footer>
+  )
+}
+
+
+export default {
+  contentHeader,
+  contentMain,
+  contentFooter,
+}
+
+//css
+//props
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import content from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div>
+      <content.contentHeader />
+      <content.contentMain />
+      <content.contentFooter />
+    </div>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
